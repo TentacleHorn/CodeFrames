bc29faf69d451cdf0a33af0998017b2413e879a3
GitHub Action <action@github.com>
2021-09-27 16:59:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/tentaclehorn/codeframes/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tentaclehorn/codeframes/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tentaclehorn.codeframes.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tentaclehorn.codeframes.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/tentaclehorn/codeframes/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tentaclehorn/codeframes/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tentaclehorn.codeframes.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tentaclehorn.codeframes.MyBundle
 
 class MyProjectService(project: Project) {
 
